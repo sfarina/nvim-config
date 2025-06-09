9b4af641123f4765747a59d002b574c38ec913b2
Salvatore Farina <sal.farina@gmail.com>
2025-06-08 23:11:07 -0400

    don't try to set python host prog

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-vim.g.python3_host_prog = "~/.asdf/installs/python/3.11.7/bin/python"
+-- vim.g.python3_host_prog = "~/.asdf/installs/python/3.11.7/bin/python"
 require("config.lazy")
 require("config.global")
 require("config.keymap")
